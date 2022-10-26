b39348346abcedde798b45cabb29077252e87e36
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-26 11:00:45 +0200

    esercizio svolto

---
+++ jsnack2/js/my-script.js
@@ -0,0 +1,9 @@
+'use strict'
+const firstWord = prompt('Inserisci la prima parola')
+const secondWord = prompt('Inserisci la seconda parola')
+
+if (firstWord.lenght > secondWord.lenght) {
+    console.log(secondWord, firstWord);
+} else {
+    console.log(secondWord, firstWord);
+}

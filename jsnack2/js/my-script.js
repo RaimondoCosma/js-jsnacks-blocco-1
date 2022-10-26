262ebac496ecd87427853065ee3e569991189627
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-26 11:51:11 +0200

    esercizio

--- jsnack2/js/my-script.js
+++ jsnack2/js/my-script.js
@@ -1,9 +1,11 @@
-'use strict'
-const firstWord = prompt('Inserisci la prima parola')
-const secondWord = prompt('Inserisci la seconda parola')
+'use strict';
+const firstWord = prompt('Inserisci la prima parola');
+const secondWord = prompt('Inserisci la seconda parola');
 
 if (firstWord.lenght > secondWord.lenght) {
     console.log(secondWord, firstWord);
+} else if (firstWord.lenght < secondWord.lenght) {
+    console.log(first, firstWord);
 } else {
-    console.log(secondWord, firstWord);
+    console.log('le parole hanno la stessa lunghezza');
 }
---
+++ jsnack3/js/my-script.js
@@ -0,0 +1,8 @@
+'use strict';
+i = 0
+for (let i = 0; i < 10; i++); {
+   let number = Number(prompt('Inserisci un numero'));
+   i = i + number
+}
+
+console.log(`Tot : ${i}`);
